c60691c375d0d2384ace777d05dd18c2602638d4
mrgarnett5-wav <mrgarnett5@gmail.com>
2021-03-04 21:25:51 -0500

    TipCalculator

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,1 +1,21 @@
-print("EAD")
+#Greeting
+print("Can't think of an appropiate tip? We're here to help")
+
+bill = float(input("Enter Total Bill: "))
+
+print("\n")
+
+number_of_people = int(input("Total amount of Guests: "))
+
+print("\n")
+
+percentage_of_tip = int(input("What percentage of tip would you like to give?"))
+
+print("\n")
+
+contributions_per_guest = ("%.2f" % round(float(((percentage_of_tip/100 +1)* bill)/number_of_people),2))
+
+#test to make sure it works
+print(f"Total contribution per guest: ${contributions_per_guest}")
+
+
